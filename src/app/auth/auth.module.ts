7dba6474da343e08c5ab8c1a6e0afaecd099a4bf
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2025-06-28 23:42:16 -0500

    Refactor module imports and declarations in auth and login
    Reordered and cleaned up imports in auth.module.ts and login.module.ts for better readability and consistency. Moved LoginComponent import to login.module.ts and adjusted declarations accordingly.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,17 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-import { MainComponent } from './pages/main/main.component';
-import { PassChangeComponent } from './pages/pass-change/pass-change.component';
-import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 
+import { AuthRoutingModule } from './auth-routing.module';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { MainComponent } from './pages/main/main.component';
+import { PassChangeComponent } from './pages/pass-change/pass-change.component';
+
 @NgModule( {
   declarations: [
-    // LoginComponent,
     MainComponent,
     PassChangeComponent,
     ForgotPasswordComponent
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from '../auth/pages/login/login.component';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from '../auth/pages/login/login.component';
+import { LoginRoutingModule } from './login-routing.module';
 
 
 @NgModule( {
